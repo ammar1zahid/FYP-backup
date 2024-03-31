073b9037cef53025e5ce34da704a402dc440a197
Areebah Binte Suhail <areebahsohail02@gmail.com>
2024-03-31 21:32:29 +0500

    Experience section of portfolio is dynamic

--- Frontend/src/portfolio/editableComponents/ExperienceP.jsx
+++ Frontend/src/portfolio/editableComponents/ExperienceP.jsx
@@ -6,9 +6,9 @@ import '../vendor/simple-line-icons/css/simple-line-icons.css'
 import '../css/resume.min.css'
 
 
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 
 function ExperienceP() {
@@ -31,6 +31,10 @@ function ExperienceP() {
         setJobItems(prevState => [...prevState, newItem]);
     };
 
+    const handleRemove = (id) => {
+        setJobItems(prevState => prevState.filter(item => item.id !== id));
+    };
+
     const handleChange = (id, e) => {
         const { name, value } = e.target;
         const updatedItems = jobItems.map(item => {
@@ -42,9 +46,9 @@ function ExperienceP() {
         setJobItems(updatedItems);
     };
 
-
   return (
     
+   
 
 <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
 
@@ -53,8 +57,10 @@ function ExperienceP() {
 
     <h2 className="mb-5">Experience</h2>
 
+  
     <div>
             {jobItems.map(item => (
+
                 <div key={item.id} className="resume-item d-flex flex-column flex-md-row mb-5">
                     {/* job related info div */}
                     <div className="resume-content mr-auto">
@@ -96,15 +102,19 @@ function ExperienceP() {
                     {/* job timeline div */}
                     <div className="resume-date text-md-right">
                         <span className="text-primary">
-                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
                                 <DatePicker />
-                            </LocalizationProvider>
+                            </LocalizationProvider> */}
+                            <input type="text" name='date' placeholder='date'/>
                         </span>
                     </div>
                     {/* job timeline div */}
+                    {/* Remove button */}
+                    <button onClick={() => handleRemove(item.id)}>Remove</button>
                 </div>
+                
             ))}
-            <button onClick={handleAddMore}>Add More</button>
+            <button onClick={handleAddMore} >Add More</button>
         </div>
 
 
@@ -114,45 +124,6 @@ function ExperienceP() {
 
 
 
-
-
-    //   {/* <div className="resume-item d-flex flex-column flex-md-row mb-5">
-    //     <div className="resume-content mr-auto">
-    //       <h3 className="mb-0">Web Developer</h3>
-    //       <div className="subheading mb-3">Intelitec Solutions</div>
-    //       <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
-    //     </div>
-    //     <div className="resume-date text-md-right">
-    //       <span className="text-primary">December 2011 - March 2013</span>
-    //     </div>
-    //   </div>
-
-
-    //   <div className="resume-item d-flex flex-column flex-md-row mb-5">
-    //     <div className="resume-content mr-auto">
-    //       <h3 className="mb-0">Junior Web Designer</h3>
-    //       <div className="subheading mb-3">Shout! Media Productions</div>
-    //       <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
-    //     </div>
-    //     <div className="resume-date text-md-right">
-    //       <span className="text-primary">July 2010 - December 2011</span>
-    //     </div>
-    //   </div>
-
-
-    //   <div className="resume-item d-flex flex-column flex-md-row">
-    //     <div className="resume-content mr-auto">
-    //       <h3 className="mb-0">Web Design Intern</h3>
-    //       <div className="subheading mb-3">Shout! Media Productions</div>
-    //       <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
-    //     </div>
-    //     <div className="resume-date text-md-right">
-    //       <span className="text-primary">September 2008 - June 2010</span>
-    //     </div>
-    //   </div>
-    
-    
-    //  */}
 
   )
   
